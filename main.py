5e0cb054b0ca0963fdab23c889dda12cb40e8d0f
Aman Verasia <amanverasia@Amans-MacBook-Air.local>
2023-07-12 14:28:39 +0530

    added try except functionality

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+# Importing the basic modules
 import logging
 import subprocess
-# List the modules
-modules_to_install = ['python-telegram-bot', 'pyautogui']
+import time
 
-# Iterate over the modules to install them using pip
-for module in modules_to_install:
-    subprocess.check_call(['pip', 'install', module])
+# List of modules required for this to work
+modules_to_install = ['python-telegram-bot', 'pyautogui']
+# Putting it in a try, except block
+try:
+    # Iterate over the modules to install them using pip
+    for module in modules_to_install:
+        subprocess.check_call(['pip', 'install', module])
+except e as Exception:
+    print(e)
+
